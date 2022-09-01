72ad82357d4fac95ad16b14be04a6bbec965d271
Rduanchen <chenyouduan@gmail.com>
2022-09-01 10:14:55 +0800

    My first commit

---
+++ 固定程式/printf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+    printf("hello the world\n");
+    int a=10;
+    printf("variable a is: %d",a);
+    return 0;
+}
---
+++ 固定程式/printf_scanf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+    printf("type a sentence:\n");
+    char a[10];
+    scanf("%s",&a);
+    printf("you just type :%s",a);
+    return 0;
+}
